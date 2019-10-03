c2e003f47355f748e5cf9b8d805a442bca2eb677
scarlett <scarlett@SHIJIAFUBA08>
2019-10-03 14:58:41 -0400

    hw4: Implement Black-Scholes formula

--- xllblack/fms_black.h
+++ xllblack/fms_black.h
@@ -7,4 +7,18 @@ namespace fms::black {
     //!!! Black put and call code goes here.
     //!!! Use the functions in fms_normal.h
 
+	inline double put(double f, double sigma, double k, double t)
+	{
+		double d2 = (log(f/ k)- pow(sigma, 2) * t / 2) / (sigma * sqrt(t));
+		double d1 = d2 + sigma * sqrt(t);
+		return k * fms::normal::cdf(-d2) - f * fms::normal::cdf(-d1);
+	}
+
+	inline double call(double f, double sigma, double k, double t)
+	{
+		double d2 = (log(f / k) - pow(sigma, 2) * t / 2) / (sigma * sqrt(t));
+		double d1 = d2 + sigma * sqrt(t);
+		return f * fms::normal::cdf(d1) - k * fms::normal::cdf(d2);
+	}
+
 } // fms::black
--- xllblack/xll_black.cpp
+++ xllblack/xll_black.cpp
@@ -6,3 +6,65 @@
 //!!! put = 3.988 to three decimal places for f = 100, sigma = 0.2, k = 100, t = 0.25.
 //!!! Also test call value for these parameters.
 
+#include "../xll12/xll/xll.h"
+#include "fms_black.h"
+
+using namespace xll;
+using namespace fms::black;
+
+static AddIn xai_black_put(
+	Function(XLL_DOUBLE, L"?xll_black_put",L"XLL_BLACK_PUT")
+	.Arg(XLL_DOUBLE, L"f", L"is the current price of the underlying asset.")
+	.Arg(XLL_DOUBLE, L"sigma", L"is the volatility of the underlying asset.")
+	.Arg(XLL_DOUBLE, L"k", L"is the strike price of the option.")
+	.Arg(XLL_DOUBLE, L"t", L"is the remaining time until the expiration of the option.")
+	.Category(L"XLL")
+	.FunctionHelp(L"Return the price of the put option determined by Black-Scholes model.")
+	.Documentation(L"This function computes the price of the put option using the Black-Scholes model.")
+);
+
+double WINAPI xll_black_put(double f, double sigma, double k, double t)
+{
+#pragma XLLEXPORT
+	return put(f, sigma, k, t);
+}
+
+#if _DEBUG
+xll::test test_xll_black_put([]() {
+	double f = 100, sigma = 0.2, k = 100, t = 0.25;
+	double result = xll_black_put(f, sigma, k, t);
+	ensure(fabs(result - 3.988) < 1e-3);
+	});
+#endif
+
+static AddIn xai_black_call(
+	Function(XLL_DOUBLE, L"?xll_black_call", L"XLL_BLACK_CALL")
+	.Arg(XLL_DOUBLE, L"f", L"is the current price of the underlying asset.")
+	.Arg(XLL_DOUBLE, L"sigma", L"is the volatility of the underlying asset.")
+	.Arg(XLL_DOUBLE, L"k", L"is the strike price of the option.")
+	.Arg(XLL_DOUBLE, L"t", L"is the remaining time until the expiration of the option.")
+	.Category(L"XLL")
+	.FunctionHelp(L"Return the price of the call option determined by Black-Scholes model.")
+	.Documentation(L"This function computes the price of the call option using the Black-Scholes model.")
+);
+
+double WINAPI xll_black_call(double f, double sigma, double k, double t)
+{
+#pragma XLLEXPORT
+	return call(f, sigma, k, t);
+}
+
+
+#if _DEBUG
+xll::test test_xll_black_call([]() {
+	double f = 100, sigma = 0.2, k = 100, t = 0.25;
+	double result = xll_black_call(f, sigma, k, t);
+	ensure(fabs(result - 3.988) < 1e-3);
+});
+#endif
+
+
+
+
+
+
