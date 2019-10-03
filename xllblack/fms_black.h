93eaf6fe5ac3bb2c0c834a61cd4ab066f33bf03b
Yizhou Gu <1842191969@qq.com>
2019-10-03 15:11:16 -0400

    yg2707

--- xllblack/fms_black.h
+++ xllblack/fms_black.h
@@ -3,7 +3,19 @@
 #include "fms_normal.h"
 
 namespace fms::black {
+	inline double put(double f, double sigma, double k, double t) {
+		double d2 = (log(f / k) - sigma * sigma * t / 2) / (sigma * sqrt(t));
+		double d1 = d2 + sigma * sqrt(t);
+		double putvalue = k * fms::normal::cdf(-d2) - f * fms::normal::cdf(-d1);
+		return putvalue;
+	}
 
+	inline double call(double f, double sigma, double k, double t) {
+		double d2 = (log(f / k) - sigma * sigma * t / 2) / (sigma * sqrt(t));
+		double d1 = d2 + sigma * sqrt(t);
+		double callvalue = f * fms::normal::cdf(d1) - k * fms::normal::cdf(d2);
+		return callvalue;
+	}
     //!!! Black put and call code goes here.
     //!!! Use the functions in fms_normal.h
 
--- xllblack/xll_black.cpp
+++ xllblack/xll_black.cpp
@@ -6,3 +6,72 @@
 //!!! put = 3.988 to three decimal places for f = 100, sigma = 0.2, k = 100, t = 0.25.
 //!!! Also test call value for these parameters.
 
+#include "fms_black.h"
+#include "../xll12/xll/xll.h"
+#include "../xll12/xll/shfb/entities.h"
+
+#ifndef CATEGORY
+#define CATEGORY L"XLL"
+#endif
+
+using namespace xll;
+
+static AddIn xai_black_put(
+	Function(XLL_DOUBLE, L"?xll_black_put", L"XLL.BLACK.PUT")
+	.Arg(XLL_DOUBLE, L"f", L"is the value of current stock price.")
+	.Arg(XLL_DOUBLE, L"sigma", L"is the volatility.")
+	.Arg(XLL_DOUBLE, L"k", L"is the strike price.")
+	.Arg(XLL_DOUBLE, L"t", L"is the time.")
+	.Category(CATEGORY)
+	.FunctionHelp(L"Return the value of the put price by BS formula.")
+	.Documentation(
+		L"This function computes the put price"
+		L"using the BS formula "
+		MATH_(L"e" SUP_(minus_ L"z" sup2_ L"/2") L"/" radic_ L"2" pi_)
+	)
+);
+double WINAPI xll_black_put(double f, double sigma, double k, double t)
+{
+#pragma XLLEXPORT
+
+	return fms::black::put(f, sigma, k, t);
+}
+
+#ifdef _DEBUG
+
+test test_black_put([] {
+	ensure(abs(xll_black_put(100, 0.2, 100, 0.25) - 3.988) <= 0.001 );
+
+	});
+
+#endif
+
+static AddIn xai_black_call(
+	Function(XLL_DOUBLE, L"?xll_black_call", L"XLL.BLACK.CALL")
+	.Arg(XLL_DOUBLE, L"f", L"is the value of current stock price.")
+	.Arg(XLL_DOUBLE, L"sigma", L"is the volatility.")
+	.Arg(XLL_DOUBLE, L"k", L"is the strike price.")
+	.Arg(XLL_DOUBLE, L"t", L"is the time.")
+	.Category(CATEGORY)
+	.FunctionHelp(L"Return the value of the call price by BS formula.")
+	.Documentation(
+		L"This function computes the call price"
+		L"using the BS formula "
+		MATH_(int_ SUB_(minus_ infin_) SUP_(L"x") L"e" SUP_(minus_ L"z" sup2_ L"/2") L" dx/" radic_ L"2" pi_)
+	)
+);
+double WINAPI xll_black_call(double f, double sigma, double k, double t)
+{
+#pragma XLLEXPORT
+	return fms::black::call(f, sigma, k, t);
+}
+
+#ifdef _DEBUG
+
+test test_black_call([] {
+	ensure(abs(xll_black_call(100, 0.2, 100, 0.25) - 3.988) <= 0.001);
+
+	});
+
+#endif
+
