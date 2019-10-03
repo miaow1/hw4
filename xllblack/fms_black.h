1a666665deb47b5b67a30b3b090c0c40db47952f
rl3107 <rl3107@columbia.edu>
2019-10-03 16:08:37 -0400

    programming for finance HW4 Ruiqing Liu UNI: rl3107

--- xllblack/fms_black.h
+++ xllblack/fms_black.h
@@ -1,10 +1,30 @@
 // fms_black.h - Black model
 #pragma once
 #include "fms_normal.h"
+#include "math.h"
 
 namespace fms::black {
 
     //!!! Black put and call code goes here.
-    //!!! Use the functions in fms_normal.h
+ // Black-Scholes Call Price
+	double BLACK_CALL(double f, double k, double t, double sigma)
+	{
+		double d = (log(f / k) + t * ( 0.5 * sigma * sigma)) / (sigma * sqrt(t));
+		double call = f * fms::normal::cdf(d) - exp(0 *t) * k * fms::normal::cdf(d - sigma * sqrt(t));
+		
+		return call;
+
+	}
+	// Black-Scholes put Price
+	double BLACK_PUT(double f, double k, double t, double sigma)
+	{
+		double d = (log(f / k) + t * ( 0.5 * sigma * sigma)) / (sigma * sqrt(t));
+		double put = exp(0 * t) * k * fms::normal::cdf(sigma * sqrt(t) - d) - f * fms::normal::cdf(-d);
+		
+		return put;
+	
+	}
+	
+	//!!! Use the functions in fms_normal.h
 
 } // fms::black
--- xllblack/xll_black.cpp
+++ xllblack/xll_black.cpp
@@ -1,8 +1,106 @@
 // xll_black.cpp
+#include "fms_black.h"
+#include "xll_black.h"
+using namespace fms::black;
+using namespace xll;
 
 //!!! Implement XLL.BLACK.PUT(f, sigma, k, t) and XLL.BLACK.CALL(f, sigma, k, t)
 //!!! Document both function.
 //!!! Test both function. 
 //!!! put = 3.988 to three decimal places for f = 100, sigma = 0.2, k = 100, t = 0.25.
 //!!! Also test call value for these parameters.
+
+// XLL.BLACK.PUT(f, sigma, k, t)
+static AddIn xai_black_put(
+	Function(XLL_DOUBLE, L"?xll_black_put", L"XLL.BLACK_PUT")
+	.Arg(XLL_DOUBLE, L"f", L"is the current option price.")
+	.Arg(XLL_DOUBLE, L"k", L"is the exercise price.")
+	.Arg(XLL_DOUBLE, L"t", L"is the maturity period.")
+	.Arg(XLL_DOUBLE, L"sigma", L"is the Volatility.")
+	.Category(L"xll")
+	.FunctionHelp(L"Return the expected put value for given option.")
+);
+double WINAPI xll_black_put(double f, double k, double t, double sigma)
+{
+#pragma XLLEXPORT
+	return BLACK_PUT(f, k, t, sigma);
+}
+#ifdef _DEBUG
+
+xll::test test_xll_black_put([]() {
+	double test_result;
+	double f = 150;
+	double k = 150;
+	double t = 0.5;
+	double sigma = 0.2;
+	test_result = BLACK_PUT(f, k, t, sigma);
+	ensure((test_result - 8.4558) < 0.001);
+	});
+
+#endif // _DEBUG
+
+// XLL.BLACK.CALL(f, sigma, k, t)
+static AddIn xai_black_call(
+	Function(XLL_DOUBLE, L"?xll_black_call", L"XLL.BLACK.CALL")
+	.Arg(XLL_DOUBLE, L"f", L"is the current option price.")
+	.Arg(XLL_DOUBLE, L"k", L"is the exercise price.")
+	.Arg(XLL_DOUBLE, L"t", L"is the maturity period.")
+	.Arg(XLL_DOUBLE, L"sigma", L"is the Volatility.")
+	.Category(L"xll")
+	.FunctionHelp(L"Return the expected call value for given option.")
+);
+double WINAPI xll_black_call(double f, double k, double t, double sigma)
+{
+#pragma XLLEXPORT
+	return BLACK_CALL(f, k, t, sigma);
+}
+
+#ifdef _DEBUG
+
+xll::test test_xll_black_put([]() {
+	double test_result;
+	double f = 150;
+	double k = 150;
+	double t = 0.5;
+	double sigma = 0.2;
+	test_result = BLACK_PUT(f, k, t, sigma);
+	ensure((test_result - 8.4558) < 0.001);
+	});
+
+#endif // _DEBUG
+//TEST BASED ON THE PARAMETERS GIVEN BY PROFESSOR.
+
+#ifdef _DEBUG
+
+xll::test test_xll_black_put([]() {
+	double test_Result;
+	double f = 100;
+	double k = 100;
+	double t = 0.25;
+	double sigma = 0.2;
+	test_Result = BLACK_PUT(f, k, t, sigma);
+	ensure((test_Result - 3.988) < 0.001);
+	});
+#endif // _DEBUG
+
+#ifdef _DEBUG
+
+xll::test test_xll_blackcall([]() {
+	double test_Result;
+	double f = 100;
+	double k = 100;
+	double t = 0.25;
+	double sigma = 0.2;
+	test_Result = BLACK_CALL(f, k, t, sigma);
+	ensure((test_Result - 3.988) < 0.001);
+	});
+#endif // _DEBUG
+
+
+
+
+
+
+
+
 
