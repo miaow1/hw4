e09faf8125a780ad028af48b97cd1c695ac9dbd5
RaymoneCao <yuchao.cao@richmond.edu>
2019-10-03 15:20:40 -0400

    HW4_update

--- xllblack/fms_black.h
+++ xllblack/fms_black.h
@@ -1,10 +1,38 @@
 // fms_black.h - Black model
 #pragma once
 #include "fms_normal.h"
+using namespace fms::normal;
 
 namespace fms::black {
 
-    //!!! Black put and call code goes here.
-    //!!! Use the functions in fms_normal.h
+	//!!! Black put and call code goes here.
+	//!!! Use the functions in fms_normal.h
+	inline double put(double F, double sigma, double K, double T)
+	{
+		if (F < 0 || K < 0 || T < 0) {
+			throw std::domain_error("fms::black::put argument : INPUT ERROR");
+		}
+
+		double d1 = (log(F / K) + (0.5 * pow(sigma, 2) * T)) / (sigma * sqrt(T));
+		double d2 = (log(F / K) - (0.5 * pow(sigma, 2) * T)) / (sigma * sqrt(T));
+
+		double res_put = (K * cdf(-d2) - F * cdf(-d1));
+
+		return res_put;
+	}
+
+	inline double call(double F, double sigma, double K, double T)
+	{
+		if (F < 0 || K < 0 || T < 0) {
+			throw std::domain_error("fms::black::call argument : INPUT ERROR");
+		}
+
+		double d1 = (log(F / K) + (0.5 * pow(sigma, 2) * T)) / (sigma * sqrt(T));
+		double d2 = (log(F / K) - (0.5 * pow(sigma, 2) * T)) / (sigma * sqrt(T));
+
+		double res_call = (F * cdf(d1) - K * cdf(d2));
+
+		return res_call;
+	}
 
 } // fms::black
--- xllblack/xll_black.cpp
+++ xllblack/xll_black.cpp
@@ -1,8 +1,103 @@
 // xll_black.cpp
 
+#include "fms_black.h"
+#include "../xll12/xll/xll.h"
+#include "../xll12/xll/shfb/entities.h"
+
+#ifndef CATEGORY
+#define CATEGORY L"XLL"
+#endif
+
+using namespace xll;
+
 //!!! Implement XLL.BLACK.PUT(f, sigma, k, t) and XLL.BLACK.CALL(f, sigma, k, t)
 //!!! Document both function.
 //!!! Test both function. 
 //!!! put = 3.988 to three decimal places for f = 100, sigma = 0.2, k = 100, t = 0.25.
 //!!! Also test call value for these parameters.
 
+static AddIn xai_black_put(
+	Function(XLL_DOUBLE, L"?xll_black_put", L"XLL.BLACK.PUT")
+	.Arg(XLL_DOUBLE, L"F", L"is the discounted future price.")
+	.Arg(XLL_DOUBLE, L"sigma", L"is the volatility of underlying assets.")
+	.Arg(XLL_DOUBLE, L"K", L"is the strike price.")
+	.Arg(XLL_DOUBLE, L"T", L"is the time to maturity.")
+	.Category(CATEGORY)
+	.FunctionHelp(L"Return the value of the price for European put option.")
+	.Documentation(
+		L"This function computes the price for European put option of maturity T on"
+		L"a futures contract with strike price K and delivery date T"
+		//MATH_(L"e" SUP_(minus_ L"z" sup2_ L"/2") L"/" radic_ L"2" pi_)
+	)
+);
+
+double WINAPI xll_black_put(double F, double sigma, double K, double T)
+{
+#pragma XLLEXPORT
+	//ensure(T >= 0);
+
+	double p = std::numeric_limits<double>::quiet_NaN();
+
+	try {
+		p = fms::black::put(F, sigma, K, T);
+	}
+	catch (const std::exception& ex) {
+		XLL_ERROR(ex.what());
+	}
+
+	return p;
+}
+
+
+#ifdef _DEBUG
+
+test test_black_put([] {
+	//fabs(xll_black_put(100, 0.2, 100, 0.25) - 3.988) < 1e-3
+	ensure(std::round(xll_black_put(100, 0.2, 100, 0.25) * 1000.0) / 1000.0 == 3.988);
+
+	});
+
+#endif
+
+static AddIn xai_black_call(
+	Function(XLL_DOUBLE, L"?xll_black_call", L"XLL.BLACK.CALL")
+	.Arg(XLL_DOUBLE, L"F", L"is the discounted future price.")
+	.Arg(XLL_DOUBLE, L"sigma", L"is the volatility of underlying assets.")
+	.Arg(XLL_DOUBLE, L"K", L"is the strike price.")
+	.Arg(XLL_DOUBLE, L"T", L"is the time to maturity.")
+	.Category(CATEGORY)
+	.FunctionHelp(L"Return the value of the price for European call option.")
+	.Documentation(
+		L"This function computes the price for European call option of maturity T on"
+		L"a futures contract with strike price K and delivery date T"
+		//MATH_(L"e" SUP_(minus_ L"z" sup2_ L"/2") L"/" radic_ L"2" pi_)
+	)
+);
+
+double WINAPI xll_black_call(double F, double sigma, double K, double T)
+{
+#pragma XLLEXPORT
+	//ensure(T >= 0);
+	double p = std::numeric_limits<double>::quiet_NaN();
+
+	try {
+		p = fms::black::call(F, sigma, K, T);
+	}
+	catch (const std::exception& ex) {
+		XLL_ERROR(ex.what());
+	}
+
+	return p;
+}
+
+
+#ifdef _DEBUG
+
+test test_black_call([] {
+	//fabs(xll_black_call(100, 0.2, 100, 0.25) - 3.988) < 1e-3
+
+	ensure(std::round(xll_black_call(100, 0.2, 100, 0.25) * 1000.0) / 1000.0 == 3.988);
+
+	});
+
+#endif
