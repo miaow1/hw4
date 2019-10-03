a878c738ea79f23c1e6cbf580f480b8a90e185e3
LZ2735 <55263722+LZ2735@users.noreply.github.com>
2019-10-03 15:03:52 -0400

    lz2735, Lu Zheng, hw4

--- xllblack/fms_black.h
+++ xllblack/fms_black.h
@@ -3,13 +3,22 @@
 #include "fms_normal.h"
 
 namespace fms::black {
-
-    //!!! Black put and call code goes here.
-    //!!! Use the functions in fms_normal.h
-	inline double pdf(double x, double mu = 0, double sigma = 1)
-	{
-		auto z = (x - mu) / sigma;
-
-		return fms::normal::cdf(2);
+	//!!! Black put and call code goes here.
+	//!!! Use the functions in fms_normal.h
+	inline double put(double f, double sigma, double k, double t) {
+		double nominator = log(f / k) + (t * sigma * sigma / 2);
+		double denominator = sigma * sqrt(t);
+		double d1 = nominator / denominator;
+		double d2 = d1 - (sigma * sqrt(t));
+		double p = (k * fms::normal::cdf(-d2)) - (f * fms::normal::cdf(-d1));
+		return p;
+	}
+	inline double call(double f, double sigma, double k, double t) {
+		double nominator = log(f / k) + (t * sigma * sigma / 2);
+		double denominator = sigma * sqrt(t);
+		double d1 = nominator / denominator;
+		double d2 = d1 - (sigma * sqrt(t));
+		double c = (f * fms::normal::cdf(d1)) - (k * fms::normal::cdf(d2));
+		return c;
 	}
 } // fms::black
--- xllblack/xll_black.cpp
+++ xllblack/xll_black.cpp
@@ -5,4 +5,59 @@
 //!!! Test both function. 
 //!!! put = 3.988 to three decimal places for f = 100, sigma = 0.2, k = 100, t = 0.25.
 //!!! Also test call value for these parameters.
+#include "fms_black.h"
+#include "../xll12/xll/xll.h"
+#include "../xll12/xll/shfb/entities.h"
+
+#ifndef CATEGORY
+#define CATEGORY L"XLL"
+#endif
+
+using namespace xll;
+static AddIn xai_black_put(
+	Function(XLL_DOUBLE, L"?xll_black_put", L"XLL.BLACK.PUT")
+	.Arg(XLL_DOUBLE, L"f", L"is the price of the underlying asset.")
+	.Arg(XLL_DOUBLE, L"sigma", L"is the volatility of returns of the underlying asset.")
+	.Arg(XLL_DOUBLE, L"k", L"is the strick price.")
+	.Arg(XLL_DOUBLE, L"t", L"is the time to maturity which is expressed in years.")
+	.Category(CATEGORY)
+	.FunctionHelp(L"Return the value of the Black Scholes of a put option.")
+	.Documentation(
+		L"This function computes the value of a put option by Black Scholes formula."
+	)
+);
+double WINAPI xll_black_put(double f, double sigma, double k, double t) {
+#pragma XLLEXPORT
+	return  fms::black::put(f, sigma, k, t);
+}
+#ifdef _DEBUG
+
+test test_black_put([] {
+	ensure(abs(xll_black_put(100, 0.2, 100, 0.25) - 3.988) < 0.001);
+	});
+
+#endif
+static AddIn xai_black_call(
+	Function(XLL_DOUBLE, L"?xll_black_call", L"XLL.BLACK.CALL")
+	.Arg(XLL_DOUBLE, L"f", L"is the price of the underlying asset.")
+	.Arg(XLL_DOUBLE, L"sigma", L"is the volatility of returns of the underlying asset.")
+	.Arg(XLL_DOUBLE, L"k", L"is the strick price.")
+	.Arg(XLL_DOUBLE, L"t", L"is the time to maturity which is expressed in years.")
+	.Category(CATEGORY)
+	.FunctionHelp(L"Return the value of the Black Scholes of a call option.")
+	.Documentation(
+		L"This function computes the value of a call option by Black Scholes formula."
+	)
+);
+double WINAPI xll_black_call(double f, double sigma, double k, double t) {
+#pragma XLLEXPORT
+	return  fms::black::call(f, sigma, k, t);
+}
+#ifdef _DEBUG
+
+test test_black_call([] {
+	ensure(abs(xll_black_call(100, 0.2, 100, 0.25) - 3.988) < 1e-3);
+	});
+
+#endif
 
