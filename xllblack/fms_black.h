ec28eec74ba91e7fb2a4aaab73a3665099bf2ab7
Haiming <haiming.ran@gmail.com>
2019-10-03 12:26:29 -0400

    me testing

--- xllblack/fms_black.h
+++ xllblack/fms_black.h
@@ -6,5 +6,10 @@ namespace fms::black {
 
     //!!! Black put and call code goes here.
     //!!! Use the functions in fms_normal.h
+	inline double pdf(double x, double mu = 0, double sigma = 1)
+	{
+		auto z = (x - mu) / sigma;
 
+		return fms::normal::cdf(2);
+	}
 } // fms::black
